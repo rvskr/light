7580513ae7fe468f11008c964d9af90d95ca59ed
voskreseniev <roman.mayber@gmail.com>
2024-05-26 17:52:30 +0300

    update

--- Ligth.py
+++ Ligth.py
@@ -18,13 +18,17 @@ class ConfigManager:
         try:
             with open(self.config_file, "r", encoding="utf-8") as file:
                 return json.load(file)
-        except (FileNotFoundError, json.JSONDecodeError):
+        except (FileNotFoundError, json.JSONDecodeError) as e:
+            print(f"Ошибка загрузки конфигурации: {e}")
             return {"num_networks": 1, "interval": 60, "message": "Кажется появился свет.", "audio_file": ""}
 
     def save_config(self, config):
-        with open(self.config_file, "w", encoding="utf-8") as file:
-            json.dump(config, file, ensure_ascii=False, indent=4)
-        self.config = config
+        try:
+            with open(self.config_file, "w", encoding="utf-8") as file:
+                json.dump(config, file, ensure_ascii=False, indent=4)
+            self.config = config
+        except Exception as e:
+            print(f"Ошибка сохранения конфигурации: {e}")
 
 class ElectricityMonitor:
     def __init__(self):
@@ -32,22 +36,29 @@ class ElectricityMonitor:
         self.iface = self.get_wireless_interface()
 
     def get_wireless_interface(self):
-        for iface in self.wifi.interfaces():
-            if iface.name() and iface.status() in [const.IFACE_DISCONNECTED, const.IFACE_SCANNING, const.IFACE_CONNECTING, const.IFACE_CONNECTED]:
-                return iface
-        raise Exception("Не найден беспроводной интерфейс")
+        try:
+            for iface in self.wifi.interfaces():
+                if iface.name() and iface.status() in [const.IFACE_DISCONNECTED, const.IFACE_SCANNING, const.IFACE_CONNECTING, const.IFACE_CONNECTED]:
+                    return iface
+            raise Exception("Не найден беспроводной интерфейс")
+        except Exception as e:
+            print(f"Ошибка при получении интерфейса: {e}")
+            return None
 
     def scan_wifi_networks(self):
-        if self.iface:
-            if self.iface.status() == const.IFACE_DISCONNECTED:
-                return 0
-            self.iface.scan()
-            time.sleep(3)
-            results = self.iface.scan_results()
-            if results:
-                return len(results)
-        return 0
-
+        try:
+            if self.iface:
+                if self.iface.status() == const.IFACE_DISCONNECTED:
+                    return 0
+                self.iface.scan()
+                time.sleep(3)
+                results = self.iface.scan_results()
+                if results:
+                    return len(results)
+            return 0
+        except Exception as e:
+            print(f"Ошибка при сканировании сетей Wi-Fi: {e}")
+            return 0
 
 class ElectricityMonitorApp:
     def __init__(self, master):
@@ -65,6 +76,7 @@ class ElectricityMonitorApp:
         self.create_widgets()
         self.monitoring = False
         self.monitor = ElectricityMonitor()
+        self.update_interface_status()
 
     def create_widgets(self):
         self.num_networks_label = ttk.Label(self.master, text="Количество Wi-Fi сетей:")
@@ -104,70 +116,95 @@ class ElectricityMonitorApp:
         self.interface_value_label = ttk.Label(self.master, textvariable=self.interface_var)
         self.interface_value_label.grid(row=4, column=1, sticky="w")
 
+        self.networks_found_label = ttk.Label(self.master, text="Найдено сетей Wi-Fi:")
+        self.networks_found_label.grid(row=5, column=0, sticky="w")
+
+        self.networks_found_var = tk.StringVar()
+        self.networks_found_var.set("Неизвестно")
+        self.networks_found_value_label = ttk.Label(self.master, textvariable=self.networks_found_var)
+        self.networks_found_value_label.grid(row=5, column=1, sticky="w")
+
         self.start_stop_button = ttk.Button(self.master, text="Старт", command=self.toggle_monitoring)
-        self.start_stop_button.grid(row=5, columnspan=2)
+        self.start_stop_button.grid(row=6, columnspan=2)
 
     def update_interface_status(self):
-        if self.monitor.iface:
-            interface_name = self.monitor.iface.name()
-            interface_status = self.monitor.iface.status()
-            if interface_status == const.IFACE_CONNECTED:
-                self.interface_var.set(f"{interface_name}: Подключен")
-            elif interface_status == const.IFACE_DISCONNECTED:
-                self.interface_var.set(f"{interface_name}: Отключен")
+        try:
+            if self.monitor.iface:
+                interface_name = self.monitor.iface.name()
+                interface_status = self.monitor.iface.status()
+                if interface_status == const.IFACE_CONNECTED:
+                    self.interface_var.set(f"{interface_name}: Подключен")
+                elif interface_status == const.IFACE_DISCONNECTED:
+                    self.interface_var.set(f"{interface_name}: Отключен")
+                else:
+                    self.interface_var.set("Неизвестно")
             else:
-                self.interface_var.set("Неизвестно")
-        else:
-            self.interface_var.set("Не найден")
+                self.interface_var.set("Не найден")
+        except Exception as e:
+            self.interface_var.set(f"Ошибка: {e}")
 
     def generate_notification_filename(self, message):
-        hash_object = hashlib.md5(message.encode())
-        return f"notification_{hash_object.hexdigest()}.mp3"
+        try:
+            hash_object = hashlib.md5(message.encode())
+            return f"notification_{hash_object.hexdigest()}.mp3"
+        except Exception as e:
+            print(f"Ошибка при генерации имени файла: {e}")
+            return "notification_error.mp3"
 
     def send_notification(self):
         message = self.config["message"]
         filename = self.config.get("audio_file", "")
         if not filename or not os.path.exists(filename):
             filename = self.generate_notification_filename(message)
-            tts = gTTS(text=message, lang='ru')
-            tts.save(filename)
-            self.config["audio_file"] = filename
-            self.config_manager.save_config(self.config)
+            try:
+                tts = gTTS(text=message, lang='ru')
+                tts.save(filename)
+                self.config["audio_file"] = filename
+                self.config_manager.save_config(self.config)
+            except Exception as e:
+                print(f"Ошибка при создании аудио файла: {e}")
         try:
             playsound(filename)
         except Exception as e:
-            print("Ошибка при воспроизведении звука:", e)
+            print(f"Ошибка при воспроизведении звука: {e}")
             print(message)
 
     def check_electricity(self):
-        num_networks = self.monitor.scan_wifi_networks()
-        if num_networks >= int(self.config["num_networks"]):
-            self.status_var.set("Включен")
-            self.send_notification()
-        else:
-            self.status_var.set("Выключен")
-        self.update_interface_status()
-        self.master.update()
+        try:
+            num_networks = self.monitor.scan_wifi_networks()
+            self.networks_found_var.set(str(num_networks))
+            if num_networks >= int(self.config["num_networks"]):
+                self.status_var.set("Включен")
+                self.send_notification()
+            else:
+                self.status_var.set("Выключен")
+            self.update_interface_status()
+            self.master.update()
+        except Exception as e:
+            self.status_var.set(f"Ошибка: {e}")
 
     def start_monitoring(self):
-        new_message = self.message_entry.get()
-        old_message = self.config["message"]
+        try:
+            new_message = self.message_entry.get()
+            old_message = self.config["message"]
 
-        self.config["num_networks"] = int(self.num_networks_entry.get())
-        self.config["interval"] = int(self.interval_entry.get())
-        self.config["message"] = new_message
+            self.config["num_networks"] = int(self.num_networks_entry.get())
+            self.config["interval"] = int(self.interval_entry.get())
+            self.config["message"] = new_message
 
-        if new_message != old_message:
-            old_audio_file = self.config.get("audio_file", "")
-            if old_audio_file and os.path.exists(old_audio_file):
-                os.remove(old_audio_file)
-            self.config["audio_file"] = ""
+            if new_message != old_message:
+                old_audio_file = self.config.get("audio_file", "")
+                if old_audio_file and os.path.exists(old_audio_file):
+                    os.remove(old_audio_file)
+                self.config["audio_file"] = ""
 
-        self.config_manager.save_config(self.config)
+            self.config_manager.save_config(self.config)
 
-        self.monitoring = True
-        self.start_stop_button.config(text="Стоп")
-        threading.Thread(target=self.monitoring_task).start()
+            self.monitoring = True
+            self.start_stop_button.config(text="Стоп")
+            threading.Thread(target=self.monitoring_task).start()
+        except Exception as e:
+            print(f"Ошибка при запуске мониторинга: {e}")
 
     def stop_monitoring(self):
         self.monitoring = False
@@ -185,9 +222,12 @@ class ElectricityMonitorApp:
             time.sleep(int(self.config["interval"]))
 
 def main():
-    root = tk.Tk()
-    app = ElectricityMonitorApp(root)
-    root.mainloop()
+    try:
+        root = tk.Tk()
+        app = ElectricityMonitorApp(root)
+        root.mainloop()
+    except Exception as e:
+        print(f"Ошибка при запуске приложения: {e}")
 
 if __name__ == "__main__":
     main()
