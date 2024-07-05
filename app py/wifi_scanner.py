fa128cbb048a3d62faa06d6e4baee61a5f17cc6f
voskreseniev <roman.mayber@gmail.com>
2024-07-05 13:49:48 +0300

    +

--- app py/wifi_scanner.py
+++ app py/wifi_scanner.py
@@ -1,21 +1,57 @@
-import subprocess
+import time
+from pywifi import PyWiFi
 
 class WindowsWiFiScanner:
     def __init__(self):
         pass
 
+    def _perform_wifi_scan(self, iface, scan_duration=10):
+        iface.scan()  # Начинаем сканирование
+        time.sleep(scan_duration)  # Ожидаем завершения сканирования
+        results = iface.scan_results()  # Получаем результаты сканирования
+        return results
+
     def scan_wifi_networks(self):
         try:
-            result = subprocess.run(
-                ["netsh", "wlan", "show", "networks"],
-                capture_output=True, text=True, check=True, encoding="cp866",
-                creationflags=subprocess.CREATE_NO_WINDOW  # Добавляем этот параметр для скрытия консоли
-            )
-            networks = [line for line in result.stdout.split('\n') if "SSID" in line]
-            return len(networks)
-        except subprocess.CalledProcessError as e:
-            print(f"Ошибка при выполнении команды netsh: {e}")
-            return 0
+            wifi = PyWiFi()
+            iface = wifi.interfaces()[0]  # Получаем первый интерфейс WiFi
+            results = self._perform_wifi_scan(iface)
+
+            # Используем множество для уникальных SSID
+            unique_networks = set()
+            for network in results:
+                if network.ssid:
+                    unique_networks.add(network.ssid)
+
+            num_networks = len(unique_networks)
+            print(f"Найдено уникальных Wi-Fi сетей: {num_networks}")
+
+            return num_networks  # Возвращаем количество уникальных SSID
         except Exception as e:
             print(f"Общая ошибка: {e}")
             return 0
+
+    def scan_wifi(self, iface, known_ssids):
+        try:
+            results = self._perform_wifi_scan(iface)
+
+            new_ssids = []
+            for network in results:
+                if network.ssid not in known_ssids:
+                    known_ssids.add(network.ssid)
+                    new_ssids.append(network.ssid)
+                    print(f"Найдена новая сеть: {network.ssid}")
+
+            return new_ssids, len(known_ssids)  # Возвращаем количество новых SSID и общее количество известных SSID
+        except Exception as e:
+            print(f"Общая ошибка при сканировании Wi-Fi: {e}")
+            return [], len(known_ssids)  # Возвращаем пустой список новых SSID в случае ошибки
+
+    def monitor_wifi(self):
+        # Ваша логика мониторинга Wi-Fi здесь
+        pass
+
+if __name__ == "__main__":
+    scanner = WindowsWiFiScanner()
+    num_networks = scanner.scan_wifi_networks()
+    print(f"Количество найденных Wi-Fi сетей: {num_networks}")
